887491758ab089c3f56f48cec8855068c4465944
Sandy Sanjaya <sandyyysanjayaaa@gmail.com>
2024-02-02 18:11:11 +0700

    day 5/90

---
+++ 5-willYouMakeIt.js
@@ -0,0 +1,15 @@
+// Hari kelima 2024 coding challange
+// Will you make it 
+// https://www.codewars.com/kata/5861d28f124b35723e00005e/train/javascript
+
+// const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+//    if (distanceToPump == mpg * fuelLeft){
+//     return true; 
+//    } else{
+//     return false; 
+//    }
+
+// };
+
+const zeroFuel = (d, m, f) => d <= m * f; 
+console.log(zeroFuel(200, 50, 3)); 
